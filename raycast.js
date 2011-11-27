77740be2b81cab1e99d22284614c3a87b869f133
scarunkk <scarunkk@gmail.com>
2011-11-27 17:48:44 +0100

    Hide functions from user

--- raycast.js
+++ raycast.js
@@ -9,68 +9,86 @@ var STORE_LIN = SIZE_Y;
 
 var sheet;
 
-function onOpen() {
-  spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  var subMenus = [
-      {name:"Reset",functionName:"onOpen"},
-      {name:"Move forward",functionName:"up"},
-      {name:"Look left",functionName:"left"},
-      {name:"Look right",functionName:"right"},
-      {name:"Move backward",functionName:"down"},
+var map =
+  [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 0, 1, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   ];
-  spreadsheet.addMenu("sheetcaster", subMenus);
 
-  sheet = SpreadsheetApp.getActiveSheet();
-  initSheet();
-  raycast();
+function readMap_(x, y) {
+  return (map[Math.round(y)][Math.round(x)]);
 }
 
-function savePlayerToSheet() {
+function Sheetcast() {
+  spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var subMenus = [
+      {name:"Reset",functionName:"Sheetcast"},
+      {name:"Move forward",functionName:"up_"},
+      {name:"Look left",functionName:"left_"},
+      {name:"Look right",functionName:"right_"},
+      {name:"Move backward",functionName:"down_"},
+  ];
+  spreadsheet.addMenu("Sheetcaster", subMenus);
+
+  sheet = SpreadsheetApp.getActiveSheet();
+  initSheet_();
+  raycast_();
+}
+
+function savePlayerToSheet_() {
   sheet.getRange(STORE_LIN, 1, 1, 1).setValue(gX0);
   sheet.getRange(STORE_LIN, 2, 1, 1).setValue(gY0);
   sheet.getRange(STORE_LIN, 3, 1, 1).setValue(gA);
 }
 
-function initPlayerFromSheet() {
+function initPlayerFromSheet_() {
   var sheet = SpreadsheetApp.getActiveSheet();
   gX0 = sheet.getRange(STORE_LIN, 1, 1, 1).getValue();
   gY0 = sheet.getRange(STORE_LIN, 2, 1, 1).getValue();
   gA = sheet.getRange(STORE_LIN, 3, 1, 1).getValue();
 }
 
-function right() {
-  initPlayerFromSheet();
+function right_() {
+  initPlayerFromSheet_();
   gA = gA - 0.25;
   if (gA < 0) {
-    gA = gA + (2 * Math.PI);
+    gA += (2 * Math.PI);
   }
-  raycast();
+  raycast_();
 }
 
-function left() {
-  initPlayerFromSheet();
+function left_() {
+  initPlayerFromSheet_();
   gA = gA + 0.25;
   if (gA > (2 * Math.PI)) {
-    gA = gA - (2 * Math.PI);
+    gA -= (2 * Math.PI);
   }
-  raycast();
+  raycast_();
 }
 
-function up() {
-  initPlayerFromSheet();
+function up_() {
+  initPlayerFromSheet_();
   gX0 += Math.cos(gA) / 4;
   gY0 += Math.sin(gA) / 4;
-  raycast();
+  raycast_();
 }
 
-function down() {
-  initPlayerFromSheet();
+function down_() {
+  initPlayerFromSheet_();
   gX0 -= Math.cos(gA) / 4;
   gY0 -= Math.sin(gA) / 4;
-  raycast();
+  raycast_();
 }
 
-function initSheet() {
+function initSheet_() {
   var sheet = sheet = SpreadsheetApp.getActiveSheet();
 
   if(SIZE_X > sheet.getMaxColumns()) {
@@ -100,7 +118,7 @@ function initSheet() {
   sheet.clear();
 }
 
-function calcul_wall(vec, x) {
+function computeWall_(vec, x) {
   var cos = Math.cos(gA);
   var sin = Math.sin(gA);
   var y1 = SIZE_X / 2;
@@ -110,30 +128,12 @@ function calcul_wall(vec, x) {
   vec.y = 0.5 * sin + y1 * cos;
 }
 
-var tab =
-  [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 0, 1, 1, 0, 1, 0, 1],
-    [1, 0, 0, 0, 1, 0, 0, 1, 0, 1],
-    [1, 0, 0, 0, 1, 0, 0, 1, 0, 1],
-    [1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-  ];
-
-function check_map(x, y) {
-  return (tab[Math.round(y)][Math.round(x)]);
-}
-
-function calc_inter(vec) {
+function getWallDist_(vec) {
   var x = gX0;
   var y = gY0;
   var k = 0;
 
-  while (Math.round(check_map(y, x)) != 1 && k < 10) {
+  while (Math.round(readMap_(y, x)) != 1 && k < 10) {
     x = gX0 + k * vec.x;
     y = gY0 + k * vec.y;
     k = k + 0.01;
@@ -141,29 +141,19 @@ function calc_inter(vec) {
   return (k);
 }
 
-function getColor(k) {
-  Logger.log(k);
-    return [
-      "#BBBBBB",
-      "#AAAAAA",
-      "#999999",
-      "#888888",
-      "#777777",
-      "#666666",
-      "#555555",
-      "#444444",
-      "#333333",
-      "#222222",
-      "#111111",
-    ][Math.round(k)];
-}
-
-function draw_background(x) {
+function drawBackground_() {
   sheet.getRange(1, 1, sheet.getMaxRows() / 2, sheet.getMaxColumns()).setBackgroundColor("#DEE6FF");
   sheet.getRange(sheet.getMaxRows() / 2, 1, sheet.getMaxRows() / 2 + ((sheet.getMaxRows() + 1) % 2), sheet.getMaxColumns()).setBackgroundColor("#BBAB9E");
 }
 
-function draw_line_wall(x, k) {
+function getColor_(k) {
+  k = k / 11;
+  comp = Math.round(0xff - k * 0xff);
+  color = (comp << 16) | (comp << 8) | (comp);
+  return "#" + color.toString(16);
+}
+
+function drawWallX_(x, k) {
   var size;
 
   if (k <= 0)
@@ -177,24 +167,24 @@ function draw_line_wall(x, k) {
     size = SIZE_Y / 2;
 
   size = Math.round(size);
-  color = getColor(k);
+  color = getColor_(k);
   sheet.getRange((SIZE_Y / 2), x + 1, size, 1).setBackgroundColor(color);
   sheet.getRange((SIZE_Y / 2) - size, x + 1, size, 1).setBackgroundColor(color);
 }
 
-function Vector() {
+function Vector_() {
   this.x = 0;
   this.y = 1;
 }
 
-function raycast() {
+function raycast_() {
   sheet = SpreadsheetApp.getActiveSheet();
-  var vec = new Vector();
-  draw_background(x);
+  var vec = new Vector_();
+  drawBackground_();
   for (var x = 0; x < SIZE_X; x++) {
-    calcul_wall(vec, x);
-    var k = calc_inter(vec);
-    draw_line_wall(x, k);
+    computeWall_(vec, x);
+    var k = getWallDist_(vec);
+    drawWallX_(x, k);
   }
-  savePlayerToSheet();
+  savePlayerToSheet_();
 }
